378232c8f9af9c0cf8e17a3446b62a0715963fb0
vivekds19 <dsvivek092@gmail.com>
2025-08-19 15:23:49 +0530

    updated the firbase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: "hrental-dc8b6.firebaseapp.com",
+  projectId: "hrental-dc8b6",
+  storageBucket: "hrental-dc8b6.firebasestorage.app",
+  messagingSenderId: "772460800387",
+  appId: "1:772460800387:web:1e8aff99cde7578356eeb6",
+  measurementId: "G-ZZ5H21CYSV",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
